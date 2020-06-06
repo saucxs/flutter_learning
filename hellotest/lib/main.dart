db4f0e72bdd67c0e0e0a8c547bd85649cad71780
chengxinsong <chengxinsong@bytedance.com>
2020-06-06 19:29:13 +0800

    feat: add state management

--- hellotest/lib/main.dart
+++ hellotest/lib/main.dart
@@ -22,6 +22,17 @@ class MyApp extends StatelessWidget {
         "bg_page_2": (context) => BgPage2(),
         "stalessWidget_page_3": (context) => StatelessWidgetPage3(),
         "context_1": (context) => Context1(),
+        "counter": (context) {
+          return CounterWidget();
+          // return Text("saucxs");
+        },
+        "state_management_A": (context) => StateMangementA(),
+        "state_management_B": (context) {
+          return ParentWidget();
+        },
+        "state_management_C": (context) {
+          return ParentWidgetC();
+        },
         "/":(context) => MyHomePage(title: 'Fultter Demo'), //注册首页路由
       },
     );
@@ -99,18 +110,46 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushNamed(context, 'bg_page_2');
               },
             ),
-            FlatButton(
-              child: Text('StatelessWidget'),
-              textColor: Colors.blue,
-              onPressed: () {
-                Navigator.pushNamed(context, 'stalessWidget_page_3');
-              },
-            ),
+            // FlatButton(
+            //   child: Text('StatelessWidget'),
+            //   textColor: Colors.blue,
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, 'stalessWidget_page_3');
+            //   },
+            // ),
             FlatButton(
               child: Text('Context1'),
               textColor: Colors.blue,
               onPressed: () {
                 Navigator.pushNamed(context, 'context_1');
+              },
+            ),
+            FlatButton(
+              child: Text('counter'),
+              textColor: Colors.blue,
+              onPressed: () {
+                Navigator.pushNamed(context, 'counter');
+              },
+            ),
+            FlatButton(
+              child: Text('状态管理A'),
+              textColor: Colors.blue,
+              onPressed: () {
+                Navigator.pushNamed(context, 'state_management_A');
+              },
+            ),
+            FlatButton(
+              child: Text('状态管理B'),
+              textColor: Colors.blue,
+              onPressed: () {
+                Navigator.pushNamed(context, 'state_management_B');
+              },
+            ),
+            FlatButton(
+              child: Text('状态管理C'),
+              textColor: Colors.blue,
+              onPressed: () {
+                Navigator.pushNamed(context, 'state_management_C');
               },
             )
           ],
@@ -282,4 +321,278 @@ class Context1 extends StatelessWidget {
       ),
     );
   }
+}
+
+
+// State生命周期
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({
+    Key key,
+    this.initValue: 0,
+  });
+
+  final int initValue;
+
+  @override
+  _CounterWidgetState createState() => new _CounterWidgetState();
+}
+
+// _CounterWidgetState
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    // 初始化状态
+    _counter=widget.initValue;
+    print('initState');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          child: Text('$_counter'),
+          // 点击后计数器自增
+          onPressed: () => setState(() => ++_counter),
+        )
+      ),
+    );
+  }
+
+  @override
+  void didUpdateWidget(CounterWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('didUpdateWidget');
+  }
+
+  @override 
+  void deactivate() {
+    super.deactivate();
+    print('deactivate');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print('reassemble');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('didChangeDependencies');
+  }
+
+}
+
+
+// StateMangementA 管理自身的状态
+class StateMangementA extends StatefulWidget {
+  StateMangementA({Key key}) : super(key: key);
+  @override
+  _StateMangementAState createState() => new _StateMangementAState();
+}
+
+class _StateMangementAState extends State<StateMangementA> {
+  bool _active = false;
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        color: Colors.white,
+        child: new Center(
+           child: new ClipRRect(
+            borderRadius: const BorderRadius.all(const Radius.circular(30.0)),
+            child: new Container(
+              width: 180.0,
+              height: 180.0,
+              color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+              child: new Center(
+                child: new Text(
+                  _active ? 'Active' : 'Inactive',
+                  style: new TextStyle(fontSize: 32.0, color: Colors.white),
+                )
+              )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// 父widget 为 StateMangementB 管理状态
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => new _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new StateMangementB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      )
+    );
+  }
+}
+
+
+// StateMangementB
+class StateMangementB extends StatelessWidget {
+  StateMangementB({ Key key, this.active: false, @required this.onChanged }) : super(key: key);
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+ Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        color: Colors.white,
+        child: new Center(
+           child: new ClipRRect(
+            borderRadius: const BorderRadius.all(const Radius.circular(30.0)),
+            child: new Container(
+              width: 180.0,
+              height: 180.0,
+              color: active ? Colors.lightGreen[700] : Colors.grey[600],
+              child: new Center(
+                child: new Text(
+                  active ? 'Active' : 'Inactive',
+                  style: new TextStyle(fontSize: 32.0, color: Colors.white),
+                )
+              )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+ 
+// 管理「混合」状态  StateManagementC
+class ParentWidgetC extends StatefulWidget {
+  @override
+  _ParentWidgetCState createState() => new _ParentWidgetCState();
+}
+
+class _ParentWidgetCState extends State<ParentWidgetC> {
+  bool _active = false;
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new StateManagementC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+
+// StateManagementC
+class StateManagementC extends StatefulWidget {
+  StateManagementC({ Key key, this.active: false, @required this.onChanged}) : super(key: key);
+  final bool active;
+  final ValueChanged<bool> onChanged;
+  @override
+  _StateManagementCState createState() => new _StateManagementCState();
+}
+
+class _StateManagementCState extends State<StateManagementC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      print('$_highlight down');
+      _highlight = true;
+    });
+  }
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+       print('$_highlight up');
+      _highlight = false;
+    });
+  }
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  BoxDecoration myBoxDecoration(_active, _highlight) {
+  return BoxDecoration(
+    color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+    borderRadius: const BorderRadius.all(const Radius.circular(30.0)),
+    border: _highlight ? new Border.all( color: Colors.teal[700], width: 10.0,) : null,
+  );
+}
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTapDown: _handleTapDown, // 处理按下事件
+      onTapUp: _handleTapUp, // 处理抬起事件
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: new Container(
+        color: Colors.white,
+        child: new Center(
+           child: new ClipRRect(
+            borderRadius: const BorderRadius.all(const Radius.circular(30.0)),
+            child: new Container(
+              width: 180.0,
+              height: 180.0,
+              decoration: myBoxDecoration(widget.active, _highlight),
+              child: new Center(
+                child: new Text(
+                  widget.active ? 'Active' : 'Inactive',
+                  style: new TextStyle(fontSize: 32.0, color: Colors.white),
+                )
+              )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
 }
